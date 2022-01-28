f26ad22416ac8b69c6200b71cca1ae05b949d9ea
chenshuideshizi <fanwenxiang326@163.com>
2022-01-28 23:39:03 +0800

    featuer:修改新闻中心

---
+++ entry/entry-tpl2.js
@@ -0,0 +1,4 @@
+import '@/template/tpl2/common/styles/common.less'
+import '@/template/tpl2/index/style.less'
+import '@/template/tpl2/product/style.less'
+import '@/template/tpl2/contact/style.less'
