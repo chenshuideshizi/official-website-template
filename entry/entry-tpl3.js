415448f081aa1ab426fd5b06d6d5652a0d1a951c
chenshuideshizi <fanwenxiang326@163.com>
2022-01-29 00:38:52 +0800

    feature:添加模板

---
+++ entry/entry-tpl3.js
@@ -0,0 +1,4 @@
+import '@/template/tpl3/common/styles/common.less'
+import '@/template/tpl3/index/style.less'
+import '@/template/tpl3/product/style.less'
+import '@/template/tpl3/contact/style.less'
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import './less/common.less'
-
-import '@/pages/index/style.less'
-import '@/pages/product/style.less'
-import '@/pages/contact/style.less'
