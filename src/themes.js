98c4d442dcb28ec8e0576971672da867017e54c6
fanwenxiang <fanwenxiang@aiforward.com>
2022-02-08 14:36:31 +0800

    feature:临时保存

---
+++ src/themes.js
@@ -0,0 +1,2 @@
+// Generate by Script, Config file is themes.config.js
+import './less/themes/green.less';import './less/themes/red.less';import './less/themes/yellow.less';import './less/themes/purple.less';
