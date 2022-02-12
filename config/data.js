725aa0ae3dc6d7f5458538b5b87b7ae38d07131b
chenshuideshizi <fanwenxiang326@163.com>
2022-02-12 15:11:06 +0800

    feature:添加模板

--- config/data.js
+++ config/data.js
@@ -1,16 +1,10 @@
 module.exports = {
-    nav: [{
-      text: '网站首页',
-      en: 'HOME',
-      path: 'index'
-    }, {
-      text: '联系我们',
-      en: 'CONTACT US',
-      path: 'contact'
-    }, {
-      text: '产品展示',
-      en: 'PRODUCT',
-      path: 'product'
-    }]
+    nav: [
+      {
+        text: '网站首页',
+        en: 'HOME',
+        path: 'index'
+      }
+    ]
   }
   
---
+++ src/template-03/theme-change.js
@@ -0,0 +1,33 @@
+const createLink = (() => {
+    let $link = null
+    return () => {
+        if ($link) {
+            return $link
+        }
+        $link = document.createElement('link')
+        $link.ref = 'stylesheet'
+        document.querySelector('head').appendChild($link)
+        return $link
+    }
+})()
+
+
+
+const toggleTheme = (theme) => {
+    if (theme === window.currentThemeLink && window.currentThemeLink.dataset.theme) {
+      return
+    }
+    const themeLink = document.createElement('link');
+    themeLink.rel = 'stylesheet';
+    themeLink.href = `./themes.${theme}.css`
+    themeLink.dataset.theme = theme;
+    document.head.appendChild(themeLink);
+    themeLink.onload = () => {
+        window.currentThemeLink.parentNode.removeChild(window.currentThemeLink)
+        window.currentThemeLink = themeLink
+    }
+}
+
+window.toggleTheme = toggleTheme
+toggleTheme('default')
+
