f40ac433720984e56a67d77cef1749c6f4c1bcb8
chenshuideshizi <fanwenxiang326@163.com>
2022-02-12 19:39:52 +0800

    feature:目录修改

--- src/themes.js
+++
@@ -1,2 +0,0 @@
-// Generate by Script, Config file is themes.config.js
-import './less/themes/green.less';import './less/themes/red.less';import './less/themes/yellow.less';import './less/themes/purple.less';
